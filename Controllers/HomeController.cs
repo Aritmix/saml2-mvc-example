3d59b0a35c1c52e8dd943bf2d86487e50204d0bf
Shlomy Tourjeman <shlomy@aritmix.com>
2021-10-25 16:14:30 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Saml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAML_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var samlEndpoint = "https://is.colman.ac.il/nidp/saml2/sso";
+
+            var request = new AuthRequest(
+                "https://test3.millenium.org.il",
+                "http://localhost:62571/Home/Contact"
+                );
+
+            //"https://login.colman.ac.il/AGLogout"
+            //redirect the user to the SAML provider
+            //var x = request.GetRedirectUrl(samlEndpoint);
+            return Redirect(request.GetRedirectUrl(samlEndpoint));
+            //ViewBag.Message = "Your application description page.";
+
+            //return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
